c6ebf5b77174b0a2054658d3abfbca92fe04bcd9
tindrais <irina.freitas@gmail.com>
2024-04-06 21:26:40 +0100

    More commments added

--- es.py
+++ es.py
@@ -17,7 +17,7 @@ References:
     * https://realpython.com/read-write-files-python/
     * https://realpython.com/command-line-interfaces-python-argparse/
     * https://stackoverflow.com/questions/1009860/how-can-i-read-and-process-parse-command-line-arguments
-    * https://docs.python.org/3/howto/argparse.html
+    * https://docs.python.org/3/library/argparse.html
     # https://www.geeksforgeeks.org/count-the-number-of-times-a-letter-appears-in-a-text-file-in-python/
 ----------------------------------------------------------------------
 ''' 
@@ -26,13 +26,14 @@ import wikipedia as wiki
 import os
 import argparse
 
-# FUNCTION 1
+####### FUNCTION 1 #######
 # Define function that gets the summary text of a given Wikipedia page and saves it in a txt file
 def wikipedia_page_to_txt(QUERY, FILENAME):
     # Wrap the code in a try statement in case an error occurs while fetching the Wikipedia page
     try:
         # Declare variable that will store the search query
         search_results = wiki.search(QUERY) 
+        print(search_results)
 
         # Get the summary of the first result 
         summary = wiki.summary(search_results[1],sentences=10) 
@@ -55,17 +56,18 @@ def wikipedia_page_to_txt(QUERY, FILENAME):
     # Following Python's best practices, the 'else' clause is preferred than adding additional code to the try statement,
     # as it avoids catching an exception than wasn't actually raised by Wikipedia query code
     else:
-        # Use open() function in write ('w') mode to create the txt file jointly with 'with' keyword,
-        # so that the file is properly closed after the program finishes.
-        # According to Python's official documention, encoding is recommended 
-        # whenever we're using a text mode, otherwise te default encoding is platform dependent. 
-        # "utf-8" is specified as it's the modern de-facto standard.
+        '''
+        Use open() function in write ('w') mode to create the txt file jointly with 'with' keyword,
+        so that the file is properly closed after the program finishes. According to Python's official documention, encoding is 
+        recommended whenever we're using a text mode, otherwise te default encoding is platform dependent. "utf-8" is specified 
+        as it's the modern de-facto standard.
+        '''
         with open(FILENAME, 'w', encoding='utf-8') as writer:
             writer.write(summary)
             #print(f'Wikipedia page about {query}: {FILENAME}')
 
 
-# FUNCTION 2
+####### FUNCTION 2 #######
 # Define function that counts occurrences of letter parsed in cmd line
 def count_char(FILENAME, LETTER):
     char = LETTER.lower()       # Convert letter to uppercase as Python is case-sensitive
@@ -80,25 +82,41 @@ def count_char(FILENAME, LETTER):
     print(f'The letter "{LETTER}" appears {total_count} times in {FILENAME}.')
 
 
-# MAIN CODE
-# Initialise the ArgumentParser object, will allows for cmd line arguments to be defined
+####### MAIN CODE #######
+'''
+Initialise the ArgumentParser object, will allows for cmd line arguments to be defined.
+Customise thethe parser by:
+    (1) specifying the program name that will be used in the usage message;
+    (2) defining a general description for the program and a closing message.
+The prog arg can be interpolated into the epilog string using the old-style string formatting operator %.
+Given that f-strings replace names with their values as they run, the program will crashwith a NameError.
+'''
 parser = argparse.ArgumentParser(
     prog="es.py",
-    description="This script counts the occurrences of the letter e.",
-    epilog="Thanks for using %(prog)s! :)")
+    description="This program searches a topic on Wikipedia, saves the summary in a text file, \
+    and counts the occurrences of the letter 'e'.",
+    epilog="Thanks for using %(prog)s!"
+)
 
-# Define arguments
-parser.add_argument("-f", "--file", type=str, required=False, help='text file name with extension, eg.: "moby_dick.txt"')
-parser.add_argument("-q", "--query", type=str, required=False, help="file's topic which will searched in Wikipedia")
+'''
+Define optional arguments for the filename and the Wikipedia's query with the following params:
+    (1) Set long and short form flags;
+    (2) Set required to false so that the program don't throw an error if no arguments are provided in the cmd line, 
+        but rather handle the error in the below try-except statement;
+    (3) Set metavar to empty to clean up the -h message by not showing the uppercase dest values (FILENAME and QUERY);
+    (4) Set the helper with a brief description of what the argument does.
+'''
+parser.add_argument("-f", "--filename", metavar="", required=False, help='required argument that defines the .txt filename, eg.: "moby_dick.txt"')
+parser.add_argument("-q", "--query", metavar="", required=False, help="required argument that sets the .txt topic to be searched on Wikipedia, eg: moby dick")
 args = parser.parse_args()
 
-# Declare variables 
-FILENAME = args.file  # Assign the filename provided in the cmd line to FILENAME
-QUERY = args.query    # Assign the query provided in the cmd line to QUERY
-LETTER = 'e'          # Define a default letter 
-
-# Wrap functions in -try-except statement
+# Wrap code in a try-except statement to handle errors in case arguments are not provided in the cmd line
 try:
+    # Declare constant variables 
+    FILENAME = args.filename    # Assign the filename provided in the cmd line to FILENAME using the dot notation on args
+    QUERY = args.query          # Assign the query provided in the cmd line to QUERY using the dot notation on args
+    LETTER = 'e'                # Define letter 
+
     # Call the Wikipedia function using the parsed arguments
     wikipedia_page_to_txt(QUERY, FILENAME)
 
@@ -106,4 +124,5 @@ try:
     count_char(FILENAME,LETTER)
 
 except: 
-    print('\nERROR: arguments missing.\n')
+    # Print a help message, including the program usage and information about the arguments defined with the ArgumentParser.
+    parser.print_help()
